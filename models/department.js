c4c40cabcf3bd953c5c3d3a11442c52589e6d0d6
Ramdas132 <rajadurgaramdas@gmail.com>
2024-12-02 20:17:54 -0600

    added bonus

--- models/department.js
+++ models/department.js
@@ -3,17 +3,24 @@ const mongoose = require('mongoose');
 const departmentSchema = new mongoose.Schema({
   departmentName: {
     type: String,
+    required: [true, 'Department name is required.'], 
+    minlength: [3, 'Department name must be at least 3 characters long.'], 
+    maxlength: [100, 'Department name cannot exceed 100 characters.'], 
   },
   size: {
     type: String,
+    required: [true, 'Size is required.'], 
+    enum: {
+      values: ['Small', 'Medium', 'Large'], 
+    },
   },
   budget: {
     type: Number,
     required: [true, 'Budget is required.'],
-    min: [1000, 'Budget must be at least 1000.'], 
-    max: [1000000, 'Budget cannot exceed 1000000.'] 
+    min: [1000, 'Budget must be at least 1000.'],
+    max: [1000000, 'Budget cannot exceed 1000000.']
   }
 });
 
-
 module.exports = mongoose.model('Department', departmentSchema);
+
--- routes/department.js
+++ routes/department.js
@@ -12,10 +12,6 @@ const departmentcontroller = require('../controllers/department');
 //router.post('/', departmentcontroller.department_create_post);
 router.get('/detail', departmentcontroller.department_view_one_Page);
 
-
-
-
-
 router.get('/', departmentcontroller.department_view_all_Page);
  
 // Route for creating a chocolate
@@ -23,11 +19,9 @@ router.get('/', departmentcontroller.department_view_all_Page);
  
 router.delete('/chocolates/:id', departmentcontroller.department_delete);
  
-
- 
 /* GET create chocolate page */
-router.get('/create', departmentcontroller.department_create_Page);
-router.get('/delete', departmentcontroller.department_delete_Page);
+router.get('/create',secured, departmentcontroller.department_create_Page);
+router.get('/delete',secured, departmentcontroller.department_delete_Page);
 router.get('/update', secured, departmentcontroller.department_update_Page);
  
 //router.put('/chocolates/:id', departmentcontroller.department_update);
